a799fb1acef7b8749b14595bff120f351edef9aa
MAHI-RAJ <mahiiraj2006@gmail.com>
2026-02-25 10:50:18 +0530

    UC4: Char array based

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,8 +1,32 @@
-public class PalindromeCheckerApp {
+public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App");
-        System.out.println("=== PALINDROME CHECKER APP ===");
-        System.out.println("Welcome User!");
+    System.out.println("=== PALINDROME CHECKER APP ===");
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter a word: ");
+    String input = scanner.nextLine();
+
+    String reversed = new StringBuilder(input).reverse().toString();
+    char[] chars = input.toCharArray();
+    boolean isPalindrome = true;
+
+    for (int i = 0; i < chars.length / 2; i++) {
+        if (chars[i] != chars[chars.length - 1 - i]) {
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    if (input.equalsIgnoreCase(reversed)) {
+        System.out.println(input + " is a Palindrome");
+        if (isPalindrome) {
+            System.out.println("Palindrome (Character Array Method)");
+        } else {
+            System.out.println(input + " is NOT a Palindrome");
+            System.out.println("Not a Palindrome");
+        }
+
+        scanner.close();
     }
 }
