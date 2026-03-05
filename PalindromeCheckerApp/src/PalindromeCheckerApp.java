007b989c0068cb64361f90b12666235f02914727
ddssfs <ddssfs>
2026-03-05 09:55:53 +0530

    UC8:LinkedList based palindrome checker

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,6 +1,28 @@
-public class PalindromeCheckerApp {
+// Simplified Logic for UC8
+public boolean isPalindrome(Node head) {
+    if (head == null || head.next == null) return true;
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App");
+    Node slow = head, fast = head;
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
     }
+
+
+    Node prev = null, curr = slow;
+    while (curr != null) {
+        Node nextTemp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+
+
+    Node firstHalf = head, secondHalf = prev;
+    while (secondHalf != null) {
+        if (firstHalf.data != secondHalf.data) return false;
+        firstHalf = firstHalf.next;
+        secondHalf = secondHalf.next;
+    }
+    return true;
 }
